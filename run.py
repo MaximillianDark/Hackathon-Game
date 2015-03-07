4226c83fea49ba804fb5ee7d9961321e0d3503c6
Python Jedi <t.c.hewittjr@gmail.com>
2015-03-07 14:41:29 -0600

    Got a window working, just to test other setups

--- run.py
+++ run.py
@@ -4,4 +4,31 @@
 Date: 2015-03-07
 Authors: PythonJedi, jkwiedman"""
 
+import time
+
 print("I'm alive!")
+
+## The following is just testing of ctypes module. It will be moved to lib.
+
+import ctypes
+
+class SDL_Window(ctypes.Structure):
+    _fields_ = []
+
+sdl2 = ctypes.CDLL("lib/SDL2.dll")
+
+sdl2.SDL_CreateWindow.restype = ctypes.POINTER(SDL_Window)
+sdl2.SDL_CreateWindow.argtypes = [ctypes.c_char_p,\
+                                  ctypes.c_int,\
+                                  ctypes.c_int,\
+                                  ctypes.c_int,\
+                                  ctypes.c_int,\
+                                  ctypes.c_uint]
+                                  
+sdl2.SDL_Init(0)
+
+sdl2.SDL_CreateWindow(b"Testing Window!", 100, 100, 640, 480, 0)
+
+time.sleep(5)
+
+sdl2.SDL_Quit()
