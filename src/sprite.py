ff7ebff6ded832e3d7713d25b76a61cfa4a5b6d6
Python Jedi <t.c.hewittjr@gmail.com>
2015-03-08 06:30:07 -0500

    Fleshed out sprite module
    Need to add update and render hooks

--- src/sprite.py
+++ src/sprite.py
@@ -1,13 +1,43 @@
 """Sprite module for the project."""
     
+import gfx
 
 class Sprite:
     """Base sprite class for onscreen objects."""
-    def __init__(self, img, rect, group=None):
-        
+    def __init__(self, img, rect, groups=None):
+        self.img = img # string to reference sprite in spritesheet
+        self.rect = rect
+        self.groups = []
+        if groups:
+            for g in groups:
+                g.add(self)
+    def _add(self, group):
+        if group not in self.groups:
+            self.groups.append(group)
+    def _remove(self, group):
+        if group in self.groups:
+            self.groups.remove(group)
+    
+    def kill(self):
+        for g in self.groups:
+            g.remove(self)
+    
         
 class Group:
-    #for collision and such
-    #update and #render for each sprite
-    #is empty
+    """Grouping class for nicely handling sprites"""
+    def __init__(self, sprites=None):
+        self.spritelist = []
+        if sprites:
+            for s in sprites:
+                self.add(s)
     
+    def add(self, sprite):
+        if sprite not in self.spritelist:
+            self.spritelist.append(sprite)
+        if self not in sprite.groups:
+            sprite._add(self)
+    
+    def remove(self, sprite):
+        if sprite in self.spritelist:
+            self.spritelist.remove(sprite)
+            sprite._remove(self)
