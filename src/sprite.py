572575cfbd753f19ae82f369ac4cbb899fac125b
jkwiedman <wiedmanjessica@gmail.com>
2015-03-07 20:52:12 -0600

    comments in sprite file

--- src/sprite.py
+++ src/sprite.py
@@ -3,4 +3,11 @@
 
 class Sprite:
     """Base sprite class for onscreen objects."""
-    def __init__(self, img, rect, group=None)
+    def __init__(self, img, rect, group=None):
+        
+        
+class Group:
+    #for collision and such
+    #update and #render for each sprite
+    #is empty
+    
