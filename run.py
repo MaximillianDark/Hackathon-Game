2be2789266845e82a23b74505b5e5f6d59c077b5
Python Jedi <t.c.hewittjr@gmail.com>
2015-03-08 08:52:31 -0500

    Added spritesheet and started entity

--- run.py
+++ run.py
@@ -12,26 +12,30 @@ import src.gfx as gfx
 import src.event as event
 import lib.sdl2 as sdl2 # need event constants
 
-gfx.init()
+def main():
+    gfx.init()
 
-win = gfx.Window("testing", (640, 480))
-ren = gfx.Renderer(win)
-tex = gfx.Texture(ren, "res\\game-tiles.bmp")
-rect = gfx.Rect(0, 0, 640, 480)
+    win = gfx.Window("testing", (640, 480))
+    ren = gfx.Renderer(win)
+    tex = gfx.Texture(ren, "res\\game-tiles.bmp")
+    rect = gfx.Rect(0, 0, 640, 480)
 
-ren.render(tex, rect, rect)
-ren.present()
-running = True
-e = event.Event()
-while running:
-    while event.poll(e):
-        if e.type == sdl2.SDL_QUIT:
-            running = False
-            break
+    ren.render(tex, rect, rect)
+    ren.present()
+    running = True
+    e = event.Event()
+    while running:
+        while event.poll(e):
+            if e.type == sdl2.SDL_QUIT:
+                running = False
+                break
 
-del(rect)
-del(tex)
-del(ren)
-del(win)
+    del(rect)
+    del(tex)
+    del(ren)
+    del(win)
 
-gfx.quit()
+    gfx.quit()
+    
+if __name__ == "__main__":
+    main()
